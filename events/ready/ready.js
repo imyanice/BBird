3860c59e8282cbfb95c84190a8a0a8dd6bfcdd5c
Yan-Jobs <yan@yanjobs.me>
2021-07-28 18:45:24 +0200

    :sparkles: Add ready event

---
+++ events/ready/ready.js
@@ -0,0 +1,25 @@
+const BaseEvent = require("../../base/BaseEvent");
+
+module.exports = class ReadyEvent extends BaseEvent {
+  constructor() {
+    super("ready");
+  }
+
+  async run(client) {
+    client.logger.log(
+      `Logged as ${client.user.tag} in ${client.guilds.cache.size} !`,
+      "ready"
+    ); // Console log ready
+    client.slashCmds.forEach((cmd) => {
+      client.slashCmds
+        .get(cmd.name)
+        .run(client)
+        .then(() => {
+          client.logger.log(
+            "🎉  Succesfully posted " + cmd.name + " command !",
+            "/"
+          );
+        });
+    });
+  }
+};
