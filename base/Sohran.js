acb012386f2595a62c8b7833ee1c3ffe6c14050f
Yan-Jobs <yan@yanjobs.me>
2021-07-28 16:25:55 +0200

    :truck: Rename base client file

---
+++ base/Sohran.js
@@ -0,0 +1,35 @@
+const { Client, Intents } = require("discord.js");
+const { GiveawaysManager } = require("discord-giveaways");
+
+class Sohran extends Client {
+  constructor() {
+    super({
+      intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.DIRECT_MESSAGES
+      ]
+    });
+
+    this.config = require("../config");
+    this.commands = new Map();
+    this.events = new Map();
+    this.slashCmds = new Map();
+    this.logger = require("../utils/logger");
+    this.giveawaysManager = new GiveawaysManager(this, {
+      storage: "./giveaways.json",
+      updateCountdownEvery: 10000,
+      default: {
+        botsCanWin: false,
+        exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR"],
+        embedColor: "#FF0000",
+        reaction: "🎉"
+      }
+    });
+  }
+}
+
+module.exports = Sohran;
