11d1780c45051422926fcf418d23daf77377cb22
Yan-Jobs <yan@yanjobs.me>
2021-07-28 18:58:36 +0200

    :sparkles: Add first command
    code will be updated tomorow

---
+++ commands/fun/hello.js
@@ -0,0 +1,12 @@
+const BaseCommand = require("../../base/BaseCommand");
+
+module.exports = class Hello extends BaseCommand {
+  constructor() {
+    super({
+        name: "hello"
+    });
+  }
+  async run(client, interaction) {
+    await interaction.reply("Hello world !");
+  }
+};
