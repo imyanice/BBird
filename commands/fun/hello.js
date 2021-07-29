36f3b76e78d1d1f7180a65b2dc6f5aea7ba26b8b
Yan-Jobs <yan@yanjobs.me>
2021-07-29 11:36:12 +0200

    :sparkles: Add enable params

--- commands/fun/hello.js
+++ commands/fun/hello.js
@@ -3,7 +3,8 @@ const BaseCommand = require("../../base/BaseCommand");
 module.exports = class Hello extends BaseCommand {
   constructor() {
     super({
-      name: "hello"
+      name: "hello",
+      enabled: false
     });
   }
   async run(client, interaction) {
