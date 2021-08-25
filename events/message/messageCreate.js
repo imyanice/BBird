42f345dfd25f022af08a28c3847dbb2eeb9e960d
Yan-Jobs <yan@yanjobs.me>
2021-08-25 18:26:39 +0200

    :sparkles: Add the super logger !

--- events/message/messageCreate.js
+++ events/message/messageCreate.js
@@ -1,5 +1,6 @@
 const BaseEvent = require("../../base/BaseEvent");
 const swearingChecker = require("../../functions/badwords");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = class MessageCreateEvent extends BaseEvent {
   constructor() {
@@ -9,6 +10,44 @@ module.exports = class MessageCreateEvent extends BaseEvent {
   }
 
   async run(client, message) {
-    swearingChecker(message, client);
+    if (!message.guild || message.author.bot) return; // If a message is send in a guild, or by a bot, then ignore it
+
+    swearingChecker(message, client); // If a message contain badwords
+
+    const logChannel = client.config.logChannel;
+    if (message.channel.id === logChannel || message.webhookID) return;
+
+    let msg = "";
+    if (!message.content) {
+      msg = "Aucun contenu, un glitch a été utilisé.";
+    } else {
+      msg = "`" + message.content + "`";
+    }
+    const attachments = [];
+    message.attachments.forEach((attachement) => attachments.push(attachement));
+
+    const embed = new MessageEmbed()
+      .setAuthor(
+        "Message de " + message.member.user.tag,
+        message.member.user.avatarURL({ format: "png" }),
+        message.url
+      )
+      .addField(
+        "Utilisateur :",
+        "<@" +
+          message.member.user.id +
+          "> (ID : *" +
+          message.member.user.id +
+          "*)"
+      )
+      .addField("Date et heure :", `<t:${Date.now()}:F>`)
+      .addField("Salon :", "<#" + message.channel.id + ">")
+      .addField("Message :", msg)
+      .setTimestamp()
+      .setFooter("Fait avec 💖 par Yan Jobs#0001 pour Sohran")
+      .setColor("#feab0b");
+    client.channels.cache
+      .get(logChannel)
+      .send({ embeds: [embed], files: attachments });
   }
 };
